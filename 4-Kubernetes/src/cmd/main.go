22cbf2609af8a1f1fc9c0db13de2db69c2957983
Mert <mert1greatest@gmail.com>
2026-04-16 22:15:14 +0300

    last updates

--- 4-Kubernetes/src/cmd/main.go
+++ 4-Kubernetes/src/cmd/main.go
@@ -11,7 +11,9 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
-	r.Get("/api/v1/health", func(w http.ResponseWriter, r *http.Request) {
+	r.Use(middleware.Heartbeat("/ping"))
+
+	r.Get("/api/v1/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Health!"))
 	})
 	http.ListenAndServe(":8080", r)
